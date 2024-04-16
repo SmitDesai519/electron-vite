2e22fe8f70e153e2911214c30cab912ab610f9bf
Smit Desai <desaismit1594@gmail.com>
2024-04-16 13:36:21 +0530

    electron app setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-center text-red-600">
+        Hello world!
+      </h1>
+      <Button className="flex mx-auto mt-4">Sample shadcn button</Button>
+    </div>
+  );
+}
+
+export default App;
